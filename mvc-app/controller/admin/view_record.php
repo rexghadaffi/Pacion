edbc009e36252b0fcbf07234ab206b45a7cf8b85
dlaniger <reginald_murillo@yahoo.com>
2015-07-25 15:30:13 +0800

    regi
    admin

---
+++ mvc-app/controller/admin/view_record.php
@@ -0,0 +1,14 @@
+<?php
+class view_record extends AdminViewClass
+{
+	function listall()
+	{
+		include_once "../../data-access/mysql-db.php";
+		$obj=new DataAccessLayer;
+		$this->assign("cont", $obj->select("tblcompanyuser"));		
+	}
+        
+        
+        
+}
+?>
---
+++ mvc-app/views/admin_record.php
@@ -0,0 +1,5 @@
+<?php
+include '../shared-layouts/_layout.php';
+include "../controller/admin/admin_class.php";
+new userClass();
+?>
