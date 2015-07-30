4fd3939d63bb9e34b58fa97b1339ac49d2f5787d
rejunde <jhunfunclara.rudy@gmail.com>
2015-07-30 12:48:30 +0800

    jhun
    forum category

--- mvc-app/views/view_class.php
+++ mvc-app/views/view_class.php
@@ -24,6 +24,25 @@ class ClientViewClass
 	}
 }
 
+class CategoryViewClass
+{
+	public $arr=array();
+	
+	function show()
+	{
+		extract($this->arr);
+		include_once "../views/forumcategory/".$_GET['control'].".html";
+	}
 
+	function assign($name, $value)
+	{
+		$this->arr[$name]=$value;
+	}
+
+	public function __destruct()
+	{		
+		$this->show();
+	}
+}
 
 ?>
