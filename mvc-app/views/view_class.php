154c086b3de59a4c508e885b9882a9aabbbcc237
rejunde <jhunfunclara.rudy@gmail.com>
2015-07-31 13:38:46 +0800

    jun
    tblpost update

--- mvc-app/views/view_class.php
+++ mvc-app/views/view_class.php
@@ -45,4 +45,25 @@ class CategoryViewClass
 	}
 }
 
+class PostViewClass
+{
+	public $arr=array();
+	
+	function show()
+	{
+		extract($this->arr);
+		include_once "../views/post/".$_GET['control'].".html";
+	}
+
+	function assign($name, $value)
+	{
+		$this->arr[$name]=$value;
+	}
+
+	public function __destruct()
+	{		
+		$this->show();
+	}
+}
+
 ?>
